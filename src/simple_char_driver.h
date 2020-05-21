6e059693a9e1a4727bdb8f2ef8665a16904a92e8
Ichaudry <ichaudry@mail.sfsu.edu>
2020-05-20 18:16:36 -0700

    Bug fixes

--- src/simple_char_driver.h
+++ src/simple_char_driver.h
@@ -6,6 +6,6 @@
 #define LINUX_DRIVER_SIMPLE_CHAR_DRIVER_H
 #include <linux/ioctl.h>
 
-#define IOCTL_SIMPLE_READ _IOR('q', 1, ioctl_arg_t *)
+#define IOCTL_SIMPLE_READ _IOR('q', 1, void *)
 
 #endif //LINUX_DRIVER_SIMPLE_CHAR_DRIVER_H
