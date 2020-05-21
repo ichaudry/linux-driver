c0893452f11d8841b480d8514047f297721b4130
Ichaudry <ichaudry@mail.sfsu.edu>
2020-05-20 17:02:51 -0700

    Pushing new module code.

---
+++ src/simple_char_driver.c
@@ -0,0 +1,139 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+
+
+//Keep track if device file already open
+volatile static int is_open= 0;
+
+static char message[1024];
+int num_bytes=0;
+
+ssize_t myDevice_read(struct file * filep, char __user * uOutBuff, size_t nbytes, loff_t * offp)
+{
+    printk("The %s function was invoked\n",__FUNCTION__);
+
+    //Track number of bytes already read
+    int bytes_read=0;
+
+    //Catch null offp
+    if(offp == NULL) return -EINVAL;
+
+    //If already read to the end of file there is nothing to read
+    if(*offp >= num_bytes) return 0;
+
+    while((bytes_read < nbytes) && (*offp < num_bytes))
+    {
+        put_user(message[ *offp], uOutBuff[bytes_read]);
+        *offp++;
+        bytes_read++;
+    }
+
+    return bytes_read;
+}
+
+ssize_t myDevice_write (struct file * filep, const char __user * uInBuff, size_t nbytes, loff_t * offp)
+{
+    printk("The %s function was invoked\n",__FUNCTION__);
+
+
+}
+
+
+/*
+*Is called when a process opens the device file
+*/
+int myDevice_open (struct inode * inodep, struct file * filep)
+{
+    printk("The %s function was invoked\n",__FUNCTION__);
+
+    //Check if device open
+    if(is_open){
+        printk(KERN_INFO "Error - the device is already open\n");
+        return -EBUSY
+    }
+
+    is_open=1;
+    try_module_get(THIS_MODULE);
+
+    return 0;
+}
+
+/*
+*Is called when a process closes the device file
+*/
+int myDevice_close (struct inode * inodep, struct file * filep)
+{
+    printk("The %s function was invoked\n",__FUNCTION__);
+    //Check if device open
+    if(!is_open){
+        printk(KERN_INFO "Error - the device wasn't opened\n");
+        return -EBUSY
+    }
+
+    is_open=0;
+    module_put(THIS_MODULE);
+    
+    return 0;
+}
+
+#define DEVICE_NAME "simpleCharDriver"	
+
+
+//Major name assigned to our device driver
+static int Major;		
+
+
+struct file_operations fops = {
+	read: myDevice_read,
+	write: myDevice_write,
+	open: myDevice_open,
+	release: myDevice_close
+};
+
+static int __init myModule_start (void)
+{
+    printk("The %s function was invoked\n",__FUNCTION__);
+    printk(KERN_INFO "My module has started. Hello world\n");
+
+    //Test message
+    strncpy(message, "Hello world.",1023)
+    num_bytes= strlen(message);
+
+
+    //Registering character driver. Letting kernel pick device number
+    Major= register_chrdev(0,DEVICE_NAME ,fops);
+
+    //Catch errors registering device
+	if (Major < 0) {
+	  printk(KERN_ALERT "Registering char device failed with %d\n", Major);
+	  return Major;
+	}
+
+    printk(KERN_INFO "The simple char device major number is :%d\n",deviceNum);
+
+    return 0;
+}
+
+
+static void __exit myModule_cleanup (void)
+{
+    printk("The %s function was invoked\n",__FUNCTION__);
+    printk(KERN_INFO "My module says goodbye world\n");
+    
+    /* 
+	 * Unregister the device 
+	 */
+	int ret = unregister_chrdev(Major, DEVICE_NAME);
+	if (ret < 0)
+		printk(KERN_ALERT "Error in unregister_chrdev: %d\n", ret);
+}
+
+
+module_init(myModule_start);
+module_exit(myModule_cleanup);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Ibraheem Chaudry");
+MODULE_DESCRIPTION("Simple Character Driver");
+
